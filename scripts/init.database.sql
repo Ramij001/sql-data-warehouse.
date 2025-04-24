1ebbac26eb6845821966993e65451a192acdaece
Ramij <ramijhossien@gmail.com>
2025-04-24 17:23:37 +0530

    Create init.database.sql

---
+++ scripts/init.database.sql
@@ -0,0 +1,32 @@
+/*
+CREATE DATABASE AND SCHEMAS
+============================
+Script Purpose:
+    This creates for a new database named 'DataWareHouse' after checking it already exists.
+
+Warning :
+   Running all the scripts will drop the 'DataWareHouse' database if it's exist.
+*/
+
+USE MASTER;
+GO
+-- Drop and recreate the 'DataWareHouse' database
+if exists (select 1 from sys.database where name = 'DataWareHouse')
+begin
+     alter DATABASE DataWareHouse set sigle_user with rollback immediate;
+end;
+go
+
+-- Create DataWareHouse database  
+create database DataWareHouse;
+go
+  
+use Datawarehouse;
+
+-- Create Schemas
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
+go
